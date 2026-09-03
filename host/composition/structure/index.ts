5958400bed34551e0e4b68fc32585591f68b3be9
dudududu144 <dudududi144@gmail.com>
2026-09-03 20:44:02 +0300

    feat: add composition/structure/index.ts

---
+++ host/composition/structure/index.ts
@@ -0,0 +1,5 @@
+// PSY ANTHEM - structure/index.ts
+export { sampleEnergyCurve, barEnergy } from './energy-curve';
+export { getMacroForm } from './macro-form';
+export type { FormSegment } from './macro-form';
+export { planSections } from './section-planner';
