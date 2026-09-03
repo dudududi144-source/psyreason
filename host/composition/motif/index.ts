16cc8a225723b7a8d8150c464bfc44d7026215ba
dudududu144 <dudududi144@gmail.com>
2026-09-03 20:43:49 +0300

    feat: add composition/motif/index.ts

---
+++ host/composition/motif/index.ts
@@ -0,0 +1,6 @@
+// PSY ANTHEM - motif/index.ts
+export { generateMotif, rhythmicCharacterFor } from './generator';
+export type { Contour, RhythmicCharacter } from './generator';
+export { applyTransform, transformMotifForSection } from './transformer';
+export type { TransformedMaterial } from './transformer';
+export { scoreMotif } from './scorer';
