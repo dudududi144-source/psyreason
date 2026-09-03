404da950c3d14cbee08ecfc1be075d56a85eeced
dudududu144 <dudududi144@gmail.com>
2026-09-03 20:45:15 +0300

    feat: add composition/expression/index.ts

---
+++ host/composition/expression/index.ts
@@ -0,0 +1,4 @@
+// PSY ANTHEM - expression/index.ts
+export { humanizeTiming } from './humanize';
+export { deriveArticulation } from './articulation';
+export { velocityFromEnergy } from './dynamics';
